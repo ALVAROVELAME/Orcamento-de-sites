90352abb087dabf3c34318e350bdd9463b0ed226
ALVAROVELAME <alvarovelame@gmail.com>
2026-05-23 20:38:37 -0300

    fix: corrigindo numeracao

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -51,26 +51,26 @@ class ImageItem implements ResponsiveImage {
 export const fetchPortfolioData = async (): Promise<PortfolioData> => {
   return {
     heroImage: new ImageItem("principal", "Design de destaque", 1200, 675, "100vw"),
-    extraImage: new ImageItem("extra", "Imagem extra do projeto", 800, 600),
+    extraImage: new ImageItem("extra", "Imagem extra do projeto", 800, 400),
     
     socialImages: [
-      new ImageItem("post1", "Post social 1", 400, 600),
-      new ImageItem("post2", "Post social 2", 400, 600),
-      new ImageItem("post3", "Post social 3", 400, 600),
-      new ImageItem("post4", "Post social 4", 400, 600)
+      new ImageItem("post1", "Post social 1", 400, 800),
+      new ImageItem("post2", "Post social 2", 400, 800),
+      new ImageItem("post3", "Post social 3", 400, 800),
+      new ImageItem("post4", "Post social 4", 400, 800)
     ],
     
     menuImages: [
-      new ImageItem("1", "Menu 1", 800, 600),
-      new ImageItem("2", "Menu 2", 800, 600),
-      new ImageItem("3", "Menu 3", 800, 600),
-      new ImageItem("4", "Menu 4", 800, 600)
+      new ImageItem("1", "Menu 1", 400, 800),
+      new ImageItem("2", "Menu 2", 400, 800),
+      new ImageItem("3", "Menu 3", 400, 800),
+      new ImageItem("4", "Menu 4", 400, 800)
     ],
     
     printedImages: [
-      new ImageItem("panfleto1", "Panfleto 1", 800, 1000),
-      new ImageItem("panfleto2", "Panfleto 2", 800, 1000),
-      new ImageItem("panfleto3", "Panfleto 3", 800, 1000),
+      new ImageItem("panfleto1", "Panfleto 1", 400, 800),
+      new ImageItem("panfleto2", "Panfleto 2", 400, 800),
+      new ImageItem("panfleto3", "Panfleto 3", 400, 800),
     ]
   };
 };
